7dce2a3931eec96f6ff3cc1faf987435fcdcce40
Antreas Antoniou <iam@antreas.io>
2022-11-23 21:43:03 +0000

    Update tali-kubecluster-spot-a100-gpu.sh

--- tali-kubecluster-spot-a100-gpu.sh
+++ tali-kubecluster-spot-a100-gpu.sh
@@ -1,1 +1,12 @@
-gcloud beta container --project "tali-multi-modal" clusters create "spot-gpu-cluster-1" --zone "us-central1-f" --no-enable-basic-auth --cluster-version "1.23.12-gke.100" --release-channel "regular" --machine-type "a2-highgpu-1g" --accelerator "type=nvidia-tesla-a100,count=1" --image-type "UBUNTU_CONTAINERD" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/cloud-platform" --max-pods-per-node "110" --spot --num-nodes "1" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias --network "projects/tali-multi-modal/global/networks/default" --subnetwork "projects/tali-multi-modal/regions/us-central1/subnetworks/default" --no-enable-intra-node-visibility --default-max-pods-per-node "110" --enable-autoscaling --min-nodes "0" --max-nodes "16" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver,GceFilestoreCsiDriver --enable-autoupgrade --enable-autorepair --max-surge-upgrade 10 --max-unavailable-upgrade 0 --autoscaling-profile optimize-utilization --resource-usage-bigquery-dataset "tali_billing_us" --enable-network-egress-metering --enable-resource-consumption-metering --workload-pool "tali-multi-modal.svc.id.goog" --enable-shielded-nodes --node-locations "us-central1-f"
+gcloud beta container --project "tali-multi-modal" clusters create "spot-gpu-cluster-1" --zone "us-central1-f" \
+--no-enable-basic-auth --cluster-version "1.23.12-gke.100" --release-channel "regular" --machine-type "a2-highgpu-1g" \
+--accelerator "type=nvidia-tesla-a100,count=1" --image-type "UBUNTU_CONTAINERD" --disk-type "pd-standard" --disk-size "100" \
+--metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/cloud-platform" --max-pods-per-node "110" \
+--spot --num-nodes "1" --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM --enable-ip-alias \
+--network "projects/tali-multi-modal/global/networks/default" \
+--subnetwork "projects/tali-multi-modal/regions/us-central1/subnetworks/default" --no-enable-intra-node-visibility \
+--default-max-pods-per-node "110" --enable-autoscaling --min-nodes "0" --max-nodes "16" --no-enable-master-authorized-networks \
+--addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver,GceFilestoreCsiDriver --enable-autoupgrade \
+--enable-autorepair --max-surge-upgrade 10 --max-unavailable-upgrade 0 --autoscaling-profile optimize-utilization \
+--resource-usage-bigquery-dataset "tali_billing_us" --enable-network-egress-metering --enable-resource-consumption-metering \
+--workload-pool "tali-multi-modal.svc.id.goog" --enable-shielded-nodes --node-locations "us-central1-f"

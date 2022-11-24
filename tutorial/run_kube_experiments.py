1353159af844ac573d6ac0d2e8f349ce454e1371
Antreas Antoniou <iam@antreas.io>
2022-11-24 02:04:46 +0000

    Update tutorial docker

--- tutorial/run_kube_experiments.py
+++ tutorial/run_kube_experiments.py
@@ -5,17 +5,12 @@ from rich import print
 
 
 def get_scripts():
-    template_script_path = Path("tutorial/simple.sh")
-    template_script_text = template_script_path.read_text()
-    script_storage_path = template_script_path.parent / "scripts"
-
-    if not script_storage_path.exists():
-        script_storage_path.mkdir(parents=True)
 
     script_list = []
     for i, j in itertools.product(range(1), range(1)):
-        current_script_text = copy.deepcopy(template_script_text)
-        current_script_text = current_script_text.replace("$args$", f"--i {i} --j {j}")
+        current_script_text = (
+            f"python /workspace/bwatchcompute/tutorial/simple.py --i {i} --j {j}"
+        )
         script_list.append(current_script_text)
 
     return script_list
--- tutorial/setup_secrets.sh
+++ tutorial/setup_secrets.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-kubectl create namespace wandb-secrets
-
 kubectl create secret generic wandb-vars \
-    --from-literal=WANDB_API_KEY=$WANDB_API_KEY \
-    --namespace=wandb-secrets
+    --from-literal=WANDB_API_KEY=$WANDB_API_KEY
